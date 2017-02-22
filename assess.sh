e0a11f6d92897615e040d1b021311dc16aa466ff
Sherlock Holo <sherlockya@gmail.com>
2017-02-22 14:24:55 +0800

    0.0.2

--- assess.sh
+++ assess.sh
@@ -3,9 +3,9 @@
 LOGFILE=/tmp/access.log
 
 exec  >> $LOGFILE 2>&1
-cat $LOGFILE &
-ssh root@121.42.26.187 -p 2333
-
+#cat $LOGFILE &
+ssh root@121.42.26.187 -p 2333 << EOF
+touch 1
 dnf update -y
 dnf install screenfetch -y
-
+EOF
