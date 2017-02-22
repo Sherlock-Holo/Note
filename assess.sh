f69999223e7b563df9bdcab0d5780905f698332d
VampireSH <sherlockya@gmail.com>
2017-02-22 20:42:54 +0800

    0.1

--- assess.sh
+++ assess.sh
@@ -10,6 +10,11 @@ ssh root@${ADDRESS} -p ${PORT} << EOF
 exec >> $LOGFILE 2>&1
 dnf update -y
 dnf install httpd -y
+sed -i "21 i Restart=on-failure" /usr/lib/systemd/system/httpd.service
+systemctl daemon-reload
+systemctl enable httpd
+systemctl start httpd
 EOF
 
+echo -e "Install done\n"
 scp -P $PORT root@${ADDRESS}:${LOGFILE} ./
